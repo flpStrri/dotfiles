362bb7479b86531b99b3182f61079b6c74c6588f
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2020-11-11 10:00:57 -0300

    Move from PyCharm community to professional 💸

--- homebrew.sh
+++ homebrew.sh
@@ -9,8 +9,7 @@ fi
 # Homebrew packages
 # --------
 brew install pyenv postgresql poetry gh trash mas terraform pre-commit
-brew cask install docker pycharm-ce 1password visual-studio-code fork appcleaner discord insomnia liteicon mater postico zoomus whatsapp slack amazon-chime microsoft-excel
-mv /Applications/PyCharm\ CE.app/ /Applications/PyCharm.app/
+brew cask install docker pycharm 1password visual-studio-code fork appcleaner discord insomnia liteicon mater postico zoomus whatsapp slack amazon-chime microsoft-excel
 brew install docker-compose
 brew tap homebrew/cask-fonts
 brew cask install font-jetbrains-mono 

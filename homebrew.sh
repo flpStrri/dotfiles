57c76863200499b6ea7d0dba091d69564b94be40
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2020-11-13 10:35:17 -0300

    Update dock icons and positions

--- homebrew.sh
+++ homebrew.sh
@@ -8,7 +8,7 @@ fi
 
 # Homebrew packages
 # --------
-brew install pyenv postgresql poetry gh trash mas terraform pre-commit
+brew install pyenv postgresql poetry gh trash mas terraform pre-commit dockutil
 brew cask install docker pycharm 1password visual-studio-code fork appcleaner discord insomnia liteicon mater postico zoomus whatsapp slack amazon-chime microsoft-excel netnewswire
 brew install docker-compose
 brew tap homebrew/cask-fonts

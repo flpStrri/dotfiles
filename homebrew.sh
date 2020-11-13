2bd4667e816a418d297c70d5152bd71c9b2b5af0
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2020-11-13 12:26:02 -0300

    Install Paw

--- homebrew.sh
+++ homebrew.sh
@@ -9,7 +9,7 @@ fi
 # Homebrew packages
 # --------
 brew install pyenv postgresql poetry gh trash mas terraform pre-commit dockutil duti
-brew cask install docker pycharm 1password visual-studio-code fork appcleaner discord insomnia liteicon mater postico zoomus whatsapp slack amazon-chime microsoft-excel netnewswire
+brew cask install docker pycharm 1password visual-studio-code fork appcleaner discord insomnia liteicon mater postico zoomus whatsapp slack amazon-chime microsoft-excel netnewswire paw
 brew install docker-compose
 brew tap homebrew/cask-fonts
 brew cask install font-jetbrains-mono 

650c5386525349f3f5932b7a19aba6fa2ae65358
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2020-11-13 10:57:08 -0300

    Add Net News Wire

--- homebrew.sh
+++ homebrew.sh
@@ -9,7 +9,7 @@ fi
 # Homebrew packages
 # --------
 brew install pyenv postgresql poetry gh trash mas terraform pre-commit
-brew cask install docker pycharm 1password visual-studio-code fork appcleaner discord insomnia liteicon mater postico zoomus whatsapp slack amazon-chime microsoft-excel
+brew cask install docker pycharm 1password visual-studio-code fork appcleaner discord insomnia liteicon mater postico zoomus whatsapp slack amazon-chime microsoft-excel netnewswire
 brew install docker-compose
 brew tap homebrew/cask-fonts
 brew cask install font-jetbrains-mono 

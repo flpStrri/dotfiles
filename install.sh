bb9582e787ed9f83de0db69cacd5ca969700c7b3
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2020-11-13 09:59:04 -0300

    Restore dev tools install before using `git`

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@
 DEVELOPMENT_DIR="$HOME/GitHub"
 DOTFILES_DIR="$DEVELOPMENT_DIR/dotfiles"
 
+printf "\e[1m\e[32m?\e[39m Enter your computer password to install software updates and Xcode dev tools:\e[0m\n"
+sudo softwareupdate -i -a
+xcode-select --install
+
 mkdir -p $DEVELOPMENT_DIR
 cd $DEVELOPMENT_DIR
 git clone https://github.com/flpStrri/dotfiles.git

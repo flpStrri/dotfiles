cd5f3161177d7b58a3cb9c50f8bd88bf8f9d955a
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2021-02-02 10:38:20 -0300

    Add extra manual steps

--- etc/bootstrap-macos.sh
+++ etc/bootstrap-macos.sh
@@ -12,9 +12,10 @@ chmod -R go-w "$(brew --prefix)/share"
 
 printf "\n\e[33m!\e[0m \e[1mFinish by doing this manual tasks:\e[0m\n"
     printf "%s\n" \
-    "— Install fonts from iCloud folder;" \
-    "— Update default font and theme in Terminal;" \
+    "- Install fonts from iCloud folder;" \
     "— Install Camera Settings: https://support.logi.com/hc/en-ca/articles/360049055854;" \
+    "— Install Citrix Workspace: https://www.citrix.com/downloads/workspace-app/mac/workspace-app-for-mac-latest.html;" \
+    "- Add the GPG key from 1Passowrd to GPG Keychain and activate it at Fork" \
     "— Edit Finder preferences:" \
     "    — Add ~/ to Finder sidebar;" \
     "— Update Magic Trackpad preferences (in Accessibility):" \

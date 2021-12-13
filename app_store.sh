a63bd390ef23a94e56ed46dad63fd15ae2c90c06
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2021-12-13 11:42:18 -0300

    Clean unused apps

--- app_store.sh
+++ app_store.sh
@@ -1,10 +1,6 @@
-# 1440
-mas install 1483764819 
-# Jira
-mas install 1475897096
 # Keynote
 mas install 409183694
 # Pages
 mas install 409201541
-# Xcode
-mas install 497799835
+# Flow
+mas install 1423210932
--- homebrew.sh
+++ homebrew.sh
@@ -10,6 +10,6 @@ fi
 # --------
 brew tap homebrew/cask-fonts
 brew install --cask font-jetbrains-mono 
-brew install starship zsh-autosuggestions postgresql poetry gh trash mas awscli terraform pre-commit dockutil duti
+brew install starship zsh-autosuggestions postgresql poetry gh trash mas pre-commit dockutil duti
 brew install python@3.10 python@3.9 python@3.8 python@3.7
-brew install --cask docker pycharm 1password visual-studio-code fork appcleaner mater zoom whatsapp slack amazon-chime netnewswire paw gpg pritunl meetingbar
+brew install --cask docker pycharm 1password fork appcleaner zoom slack amazon-chime netnewswire paw gpg pritunl

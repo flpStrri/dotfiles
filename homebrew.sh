182589da7d86b1c3ee2e82c586116f21da3a9919
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2022-09-24 16:31:13 -0300

    clean brew

--- homebrew.sh
+++ homebrew.sh
@@ -9,7 +9,23 @@ fi
 # Homebrew packages
 # --------
 brew tap homebrew/cask-fonts
-brew install --cask font-jetbrains-mono 
-brew install starship zsh-autosuggestions postgresql poetry gh trash mas pre-commit dockutil duti
-brew install python@3.10 python@3.9 python@3.8 python@3.7
-brew install --cask docker pycharm 1password fork appcleaner zoom slack amazon-chime paw gpg-suite-no-mail pritunl firefox meetingbar raycast
+brew install --cask font-jetbrains-mono
+brew install kitty starship zsh-autosuggestions gh trash dockutil duti cookiecutter jq difftastic
+
+brew tap homebrew/cask-drivers
+brew install logitech-g-hub
+
+brew install python@3.10 python@3.9 python@3.8 python@3.7 poetry
+brew install rust rust-analyzer
+brew install elixir elixir-ls
+brew install terraform hashicorp/tap/terraform-ls
+brew install postgresql@14
+mkdir -p /usr/local/opt/postgresql/lib
+ln -s /usr/local/opt/postgresql@14/lib/postgresql@14/libpq.5.dylib /usr/local/opt/postgresql/lib/libpq.5.dylib
+
+brew install --cask raycast 1password appcleaner slack firefox
+brew install --cask docker pycharm fork paw
+brew install helix
+
+# AB InBev Specific
+brew install --cask zoom pritunl

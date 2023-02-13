d02bb7a192220e4b84aa95293185a044ad94f6e5
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2023-02-13 12:28:07 -0300

    New tech stack

--- homebrew.sh
+++ homebrew.sh
@@ -10,22 +10,13 @@ fi
 # --------
 brew tap homebrew/cask-fonts
 brew install --cask font-jetbrains-mono
-brew install kitty starship zsh-autosuggestions gh trash dockutil duti cookiecutter jq difftastic
-
-brew tap homebrew/cask-drivers
-brew install logitech-g-hub
+brew install kitty starship zsh-autosuggestions gh trash dockutil jq difftastic httpie
 
 brew install python@3.10 python@3.9 python@3.8 python@3.7 poetry
 brew install rust rust-analyzer
-brew install elixir elixir-ls
 brew install terraform hashicorp/tap/terraform-ls
-brew install postgresql@14
-mkdir -p /usr/local/opt/postgresql/lib
-ln -s /usr/local/opt/postgresql@14/lib/postgresql@14/libpq.5.dylib /usr/local/opt/postgresql/lib/libpq.5.dylib
+brew install colima docker docker-credential-helper docker-compose minikube kubectl
 
-brew install --cask raycast 1password appcleaner slack firefox
-brew install --cask docker pycharm fork paw postman
+brew install --cask raycast 1password 1password-cli appcleaner
+brew install --cask fork paw
 brew install helix
-
-# AB InBev Specific
-brew install --cask zoom pritunl

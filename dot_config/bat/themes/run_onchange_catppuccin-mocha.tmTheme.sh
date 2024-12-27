4d155e244d4fa20bee4e982a64014fc4048c887b
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2024-12-27 20:32:58 -0300

    find bat without PATH config

--- dot_config/bat/themes/run_onchange_catppuccin-mocha.tmTheme.sh
+++ dot_config/bat/themes/run_onchange_catppuccin-mocha.tmTheme.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # catppuccin-mocha.tmTheme hash: {{ include "catppuccin-mocha.tmTheme" | sha256sum }}
+{{ if (.codespaces) }}
+/home/linuxbrew/.linuxbrew/bin/bat cache --build
+{{ else }}
 bat cache --build
+{{ end }}

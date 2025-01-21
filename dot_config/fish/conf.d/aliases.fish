ad4a12bd900385cffa82177b67752f4674cc9a1c
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2025-01-21 08:09:09 -0300

    ...

---
+++ dot_config/fish/conf.d/aliases.fish
@@ -0,0 +1,16 @@
+# Navigation
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
+
+# Better defaults
+alias ls 'ls -G'
+alias ll 'ls -lah'
+# alias cat 'bat --style=plain'
+alias grep 'grep --color=auto'
+
+# Git shortcuts
+alias g 'git'
+alias gs 'git status'
+alias gc 'git commit'
+alias gp 'git push'
+alias gl 'git pull'
---
+++ dot_config/fish/conf.d/theme.fish
@@ -0,0 +1,44 @@
+# Catppuccin Mocha theme for Fish
+set -U fish_color_normal cdd6f4
+set -U fish_color_command 89b4fa
+set -U fish_color_param f2cdcd
+set -U fish_color_keyword f38ba8
+set -U fish_color_quote a6e3a1
+set -U fish_color_redirection f5c2e7
+set -U fish_color_end fab387
+set -U fish_color_comment 7f849c
+set -U fish_color_error f38ba8
+set -U fish_color_gray 6c7086
+set -U fish_color_selection --background=313244
+set -U fish_color_search_match --background=313244
+set -U fish_color_option a6e3a1
+set -U fish_color_operator f5c2e7
+set -U fish_color_escape eba0ac
+set -U fish_color_autosuggestion 6c7086
+set -U fish_color_cancel f38ba8
+set -U fish_color_cwd f9e2af
+set -U fish_color_user 94e2d5
+set -U fish_color_host 89b4fa
+set -U fish_color_host_remote a6e3a1
+set -U fish_color_status f38ba8
+# Pager colors
+set -U fish_pager_color_progress 6c7086
+set -U fish_pager_color_prefix f5c2e7
+set -U fish_pager_color_completion cdd6f4
+set -U fish_pager_color_description 6c7086
+
+# Prompt configuration
+set -g fish_prompt_pwd_dir_length 1
+set -g fish_prompt_pwd_full_dirs 1
+
+function fish_prompt
+    set -l pwd_info (set_color cba6f7)(prompt_pwd)
+    set -l git_info
+    
+    if git rev-parse --is-inside-work-tree >/dev/null 2>&1
+        set -l git_branch (git symbolic-ref --short HEAD 2>/dev/null; or git rev-parse --short HEAD 2>/dev/null)
+        set git_info " "(set_color 6c7086)"$git_branch"
+    end
+    
+    echo -n "$pwd_info$git_info"(set_color cba6f7)" ⇒ "
+end

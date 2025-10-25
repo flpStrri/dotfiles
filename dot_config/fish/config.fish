07043e5c5a26b33bc1cf2dc40bf16d3889a7f4ac
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2025-10-25 19:17:07 -0300

    october-2025

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -8,11 +8,13 @@ set -gx LANG en_US.UTF-8
 set -gx LC_ALL en_US.UTF-8
 set -gx COLIMA_SAVE_CONFIG 0
 set -gx GOPATH $HOME/.local/share/go
+set -gx SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 
 # Path configuration
 fish_add_path /opt/homebrew/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $GOPATH/bin
+fish_add_path /Library/Developer/CommandLineTools/usr/bin
 
 # Suppress greeting
 set -U fish_greeting
